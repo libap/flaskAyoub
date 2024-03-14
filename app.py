90b481387fb7e40ec537b14fe56af6232aae46aa
libap <limandribaptiste@gmail.com>
2024-03-14 13:00:12 +0100

    front

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return render_template('landing.html')
+
+@app.route('/game')
+def game():
+    return render_template('game.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/signin')
+def signin():
+    return render_template('signin.html')
+
+@app.route('/leaderboard')
+def leaderboard():
+    return render_template('leaderboard.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
